ba141d401faaa78cacec442fe0a54910b14880f7
Lautaro Cozzani <lautaromail@gmail.com>
2018-01-30 18:06:25 -0300

    fix session

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpSentEvent, HttpHeaderResponse, HttpProgressEvent, HttpResponse, HttpUserEvent } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Session } from './session.service';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
@@ -9,10 +10,14 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor(private session: Session) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any>> {
-    const authReq = req.clone({
-      headers: req.headers.set('x-access-token',this.session.token())
-    });
-    return next.handle(authReq);
+    if (req.url.startsWith(environment.backendUrl) && this.session.token()) {
+      const authReq = req.clone({
+        headers: req.headers.set('x-access-token',this.session.token())
+      });
+      return next.handle(authReq);
+    } else {
+      return next.handle(req);
+    }
   }
 
 }
