0b017f01e2d5e5fa00eb0c05ed8bde64162fb55e
Lautaro Cozzani <lautaromail@gmail.com>
2018-02-14 10:46:13 -0300

    stats week to year

--- src/app/screens/stats/stats.component.ts
+++ src/app/screens/stats/stats.component.ts
@@ -7,22 +7,16 @@ import { Match } from '../../domain/model';
   selector: 'sj-stats',
   templateUrl: './stats.component.html',
   styles: [`
-:host p {
-  height: 100vh
+:host div {
+  height: calc(100vh - 64px)
 }
   `]
 })
 export class StatsComponent implements OnInit {
 
-  private readonly dows = ['D', 'M', 'T', 'W', 'T', 'F', 'S']
-  lastWeekData;
-  lastWeekLabels: any[];
-  lastMonthData;
-  lastMonthLabels: any[];
-  lastQuarter;
-  lastHalf;
-  lastYear;
   options = {
+    responsive: true,
+    maintainAspectRatio: false
     // scales: {
     //   xAxes: [{
     //     stacked: true
@@ -32,6 +26,7 @@ export class StatsComponent implements OnInit {
     //   }]
     // }
   };
+  charts: ChartData[];
 
   constructor(
     private matchesService: MatchesService
@@ -39,51 +34,35 @@ export class StatsComponent implements OnInit {
 
   ngOnInit() {
     this.matchesService.myMatches().subscribe(matches => {
-      const lastWeekDates = this.lastWeekDates();
-      const lastWeekDataSpare = this.buildLastWeekData(matches, lastWeekDates[0]);
-      this.lastWeekData = [{
-        data: this.completeLastWeekData(lastWeekDates, lastWeekDataSpare, curr => Math.round(curr.movingTime / 60)),
-        label: 'Moving time'
-      }, {
-        data: this.completeLastWeekData(lastWeekDates, lastWeekDataSpare, curr => Math.round((curr.elapsedTime) / 60)),
-        label: 'Elapsed time'
-      }];
-      this.lastWeekLabels = lastWeekDates.map(d => this.dows[new Date(d).getDay()]);
-
-      const lastMonthDates = this.lastMonthDates();
-      const lastMonthDataSpare = this.buildLastMonthData(matches, lastMonthDates[0]);
-      console.log('lastMonthDataSpare',lastMonthDataSpare)
-      this.lastMonthData = [{
-        data: this.completeLastMonthData(lastMonthDates, lastMonthDataSpare, curr => Math.round(curr.movingTime / 60)),
-        label: 'Moving time'
-      }, {
-        data: this.completeLastMonthData(lastMonthDates, lastMonthDataSpare, curr => Math.round((curr.elapsedTime) / 60)),
-        label: 'Elapsed time'
-      }];
-      this.lastMonthLabels = lastMonthDates.map(d => {
-        const to = new Date(d.getTime());
-        to.setDate(to.getDate() + 7);
-        return `${d.getMonth()+1}/${d.getDate()} - ${to.getMonth()+1}/${to.getDate()}`;
-      });
+      this.charts = [
+        new WeekChartData(matches),
+        new MonthChartData(matches),
+        new QuarterChartData(matches),
+        new YearChartData(matches)
+      ];
     });
   }
 
-  private normalizeDay(datetime: Date) {
-    let date = new Date(datetime);
-    date.setHours(0); date.setMinutes(0); date.setSeconds(0); date.setMilliseconds(0);
-    return date.getTime();
+}
+
+export abstract class ChartData {
+
+  data: any[];
+  labels: any[];
+
+  constructor(matches: Match[], amount: number, span: number, public title: string) {
+    const lastWeekDates = this.generateDates(amount, span);
+    const lastWeekDataSparse = this.buildData(matches, lastWeekDates[0]);
+    this.data = this.generateSeries(lastWeekDates, lastWeekDataSparse);
+    this.labels = lastWeekDates.map(d => this.buildLabel(d));
   }
 
-  private normalizeWeek(datetime: Date) {
-    let date = new Date(datetime);
-    date.setHours(0); date.setMinutes(0); date.setSeconds(0); date.setMilliseconds(0);
-    date.setDate(date.getDate() - date.getDay());
-    return date.getTime();
-  }
+  protected abstract normalizeDate(datetime: Date): number;
+  protected abstract buildLabel(date: Date): string;
 
-  private completeLastWeekData(lastWeekDates, lastWeekDataSpare, getData: (curr) => number) {
-    return lastWeekDates.map(date => {
-      let curr = lastWeekDataSpare.find(data => (data.date.getTime() - date.getTime()) === 0);
+  private completeData(dates, sparseData, getData: (curr) => number) {
+    return dates.map(date => {
+      let curr = sparseData.find(data => (data.date.getTime() - date.getTime()) === 0);
       if (curr) {
         return getData(curr);
       } else {
@@ -92,42 +71,21 @@ export class StatsComponent implements OnInit {
     });
   }
 
-  private completeLastMonthData(lastMonthDates, lastMonthDataSpare, getData: (curr) => number) {
-    return lastMonthDates.map(date => {
-      let curr = lastMonthDataSpare.find(data => (data.date.getTime() - date.getTime()) === 0);
-      if (curr) {
-        return getData(curr);
-      } else {
-        return 0;
-      }
-    });
-  }
-
-  private lastWeekDates() {
-    let lastWeekLabels = [this.normalizeDay(new Date())];
-    for (let i = 0; i < 6; i++) {
-      let date = new Date(lastWeekLabels[lastWeekLabels.length - 1]);
-      date.setDate(date.getDate() - 1);
-      lastWeekLabels.push(date.getTime());
+  protected generateDates(amount: number, span: number) {
+    let dates = [this.normalizeDate(new Date())];
+    for (let i = 0; i < (amount - 1); i++) {
+      let date = new Date(dates[dates.length - 1]);
+      date.setDate(date.getDate() - span);
+      dates.push(date.getTime());
     }
-    return lastWeekLabels.reverse().map(d => new Date(d));
+    return dates.reverse().map(d => new Date(d));
   }
 
-  private lastMonthDates() {
-    let lastWeekLabels = [this.normalizeWeek(new Date())];
-    for (let i = 0; i < 3; i++) {
-      let date = new Date(lastWeekLabels[lastWeekLabels.length - 1]);
-      date.setDate(date.getDate() - 7);
-      lastWeekLabels.push(date.getTime());
-    }
-    return lastWeekLabels.reverse().map(d => new Date(d));
-  }
-
-  private buildLastWeekData(matches: Match[], from: Date) {
+  protected buildData(matches: Match[], from: Date) {
     return _.map(
       _.groupBy(
         matches.filter(match => new Date(match.startDate).getTime() >= from.getTime()),
-        match => this.normalizeDay(match.startDate)
+        match => this.normalizeDate(match.startDate)
       ),
       (matches, date) => {
         return {
@@ -141,20 +99,105 @@ export class StatsComponent implements OnInit {
     ).sort((r1, r2) => r1.date.getTime() - r2.date.getTime());
   }
 
-  private buildLastMonthData(matches: Match[], from: Date) {
-    return _.map(
-      _.groupBy(
-        matches.filter(match => new Date(match.startDate).getTime() >= from.getTime()),
-        match => this.normalizeWeek(match.startDate)
-      ),
-      (matches, date) => {
-        return {
-          date: new Date(parseInt(date)),
-          dow: new Date(parseInt(date)).getDay(),
-          elapsedTime: _.sumBy(matches, match => match.elapsedTime),
-          movingTime: _.sumBy(matches, match => match.movingTime)
-        };
-      }
-    ).sort((r1, r2) => r1.date.getTime() - r2.date.getTime());
+  protected generateSeries(dates, sparseData) {
+    return [{
+      data: this.completeData(dates, sparseData, curr => Math.round(curr.movingTime / 60)),
+      label: 'Moving time'
+    }, {
+      data: this.completeData(dates, sparseData, curr => Math.round((curr.elapsedTime) / 60)),
+      label: 'Elapsed time'
+    }];
   }
+}
+
+export class WeekChartData extends ChartData {
+
+  private static readonly  dows = ['D', 'M', 'T', 'W', 'T', 'F', 'S']
+
+  constructor(matches: Match[]) {
+    super(matches, 7, 1, 'Week');
+  }
+
+  protected normalizeDate(datetime: Date) {
+    let date = new Date(datetime);
+    date.setHours(0); date.setMinutes(0); date.setSeconds(0); date.setMilliseconds(0);
+    return date.getTime();
+  }
+
+  protected buildLabel(date: Date): string {
+    return WeekChartData.dows[new Date(date).getDay()];
+  }
+
+}
+
+export class MonthChartData extends ChartData {
+
+  constructor(matches: Match[]) {
+    super(matches, 4, 7, 'Month');
+  }
+
+  protected normalizeDate(datetime: Date) {
+    let date = new Date(datetime);
+    date.setHours(0); date.setMinutes(0); date.setSeconds(0); date.setMilliseconds(0);
+    date.setDate(date.getDate() - date.getDay());
+    return date.getTime();
+  }
+
+  protected buildLabel(date: Date): string {
+    const to = new Date(date.getTime());
+    to.setDate(to.getDate() + 7);
+    return `${date.getMonth() + 1}/${date.getDate()} - ${to.getMonth() + 1}/${to.getDate()}`;
+  }
+
+}
+
+export class QuarterChartData extends ChartData {
+
+  constructor(matches: Match[]) {
+    super(matches, 4 * 3, 7, 'Qarter');
+  }
+
+  protected normalizeDate(datetime: Date) {
+    let date = new Date(datetime);
+    date.setHours(0); date.setMinutes(0); date.setSeconds(0); date.setMilliseconds(0);
+    date.setDate(date.getDate() - date.getDay());
+    return date.getTime();
+  }
+
+  protected buildLabel(date: Date): string {
+    return `${date.getMonth() + 1}/${date.getDate()}`;
+  }
+
+}
+
+export class YearChartData extends ChartData {
+
+  private static readonly months = ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D']
+
+  constructor(matches: Match[]) {
+    super(matches, 12, 30, 'Year');
+  }
+
+  // overrides fixing rounded months
+  protected generateDates(amount: number, span: number) {
+    let dates = [this.normalizeDate(new Date())];
+    for (let i = 0; i < (amount - 1); i++) {
+      let date = new Date(dates[dates.length - 1]);
+      date.setMonth(date.getMonth() - 1);
+      dates.push(date.getTime());
+    }
+    return dates.reverse().map(d => new Date(d));
+  }
+
+  protected normalizeDate(datetime: Date) {
+    let date = new Date(datetime);
+    date.setHours(0); date.setMinutes(0); date.setSeconds(0); date.setMilliseconds(0);
+    date.setDate(1);
+    return date.getTime();
+  }
+
+  protected buildLabel(date: Date): string {
+    return YearChartData.months[date.getMonth()];
+  }
+
 }
